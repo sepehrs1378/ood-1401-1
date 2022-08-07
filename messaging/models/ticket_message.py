0ca6e98b17a2141e5097d769e45ccf3988edcb21
Sepehr Safari <sepehr.safari8731@gmail.com>
2022-08-07 20:07:39 +0430

    Resolve forgotten conflict in messaging/models/ticket_messag.py.

--- messaging/models/ticket_message.py
+++ messaging/models/ticket_message.py
@@ -13,30 +13,11 @@ class TicketMessage(models.Model):
     """
 
     ticket = models.ForeignKey(
-<<<<<<< HEAD
         TicketChannel, on_delete=models.CASCADE, blank=False, null=False
     )
     text = models.TextField()
     sender = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE)
     time = models.DateField(default=now)
-=======
-        Ticket,
-        on_delete=models.CASCADE,
-        blank=True,
-        null=True,
-        default=None,
-    )
-    text = models.TextField()
-    file = models.FileField()
-    time = models.DateField(default=now)
-    sender = models.ForeignKey(
-        User,
-        blank=True,
-        null=True,
-        default=None,
-        on_delete=models.CASCADE,
-    )
->>>>>>> master
 
     def __str__(self) -> str:
         return f"message: {self.text}"
