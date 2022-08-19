09bd9da0ccd4d04402efb63c9df6abe1d5c934ce
Sepehr Safari <sepehr.safari8731@gmail.com>
2022-08-19 20:31:22 +0430

    Remove english labels from create_ticket form.

--- messaging/view/forms.py
+++ messaging/view/forms.py
@@ -2,8 +2,14 @@ from django import forms
 
 
 class TicketForm(forms.Form):
-    title = forms.CharField(max_length=30)
-    text = forms.CharField(max_length=500)
+    title = forms.CharField(
+        max_length=30, widget=forms.TextInput(attrs={"placeholder": "تیتر"})
+    )
+    text = forms.CharField(
+        max_length=500, widget=forms.TextInput(attrs={"placeholder": "متن"})
+    )
 
     def __init__(self, *args, **kwargs):
         super(TicketForm, self).__init__(*args, **kwargs)
+        self.fields["title"].label = ""
+        self.fields["text"].label = ""
